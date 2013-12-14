0484d9ae102ff335112f891cffcc8b833a32bc77
James Reeves <jreeves@weavejester.com>
2013-12-14 09:54:55 -0500

    Move person shape into person.shape

--- src/main.js
+++ src/main.js
@@ -1,14 +1,15 @@
 window.onload = function() {
   var stage = new createjs.Stage("game");
   person = createPerson();
-  stage.addChild(person);
+  stage.addChild(person.shape);
   stage.update();
 };
 
 function createPerson() {
-  var person = new createjs.Shape();
-  person.graphics.beginFill("red").drawCircle(0, 0, 50);
-  person.x = 100;
-  person.y = 100;
+  var person = {};
+  person.shape = new createjs.Shape();
+  person.shape.graphics.beginFill("red").drawCircle(0, 0, 50);
+  person.shape.x = 100;
+  person.shape.y = 100;
   return person;
 }
