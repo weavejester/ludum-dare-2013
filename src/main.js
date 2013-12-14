10c737e6224c49e3e8b8cacf15e91b50c7411463
James Reeves <jreeves@weavejester.com>
2013-12-14 09:31:44 -0500

    Initialised Easel.js

---
+++ src/main.js
@@ -0,0 +1,11 @@
+window.onload = function() {
+  var stage = new createjs.Stage("game");
+  
+  var circle = new createjs.Shape();
+  circle.graphics.beginFill("red").drawCircle(0, 0, 50);
+  circle.x = 100;
+  circle.y = 100;
+  stage.addChild(circle);
+  
+  stage.update();
+};
