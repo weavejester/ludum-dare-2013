65e6d92edd1c8439ba43665d074dded6d0e58659
James Reeves <jreeves@weavejester.com>
2013-12-14 17:25:22 -0500

    Confine person x and y to the game window

--- src/person.js
+++ src/person.js
@@ -1,5 +1,6 @@
-var Person = function(position, velocity, radius, color) {
-  this.velocity = velocity;
+var Person = function(position, radius, color) {
+  this.radius = radius;
+  this.velocity = {x: 0, y: 0};
 
   this.shape = new createjs.Shape();
   this.shape.graphics.beginFill(color).drawCircle(0, 0, radius);
@@ -10,9 +11,27 @@ var Person = function(position, velocity, radius, color) {
   createjs.Ticker.addEventListener('tick', function(e) { person.update(e.delta * 0.001); });
 };
 
+// Don't let a number get higher than max or lower than min
+function confine(value, min, max) {
+  if (value < min) {
+    return min;
+  } else if (value > max) {
+    return max;
+  } else {
+    return value;
+  }
+}
+
+// Stop a coordinate from getting out of bounds
+function confineCoord(coord, bounds) {
+  coord.x = confine(coord.x, bounds.x, bounds.x + bounds.w);
+  coord.y = confine(coord.y, bounds.y, bounds.y + bounds.h);
+}
+
 Person.prototype.update = function(delta) {
   this.shape.x += this.velocity.x * delta;
   this.shape.y += this.velocity.y * delta;
+  confineCoord(this.shape, this.bounds);
 };
 
 // returns a random colour string for beginFill() and other methods.
@@ -24,8 +43,11 @@ function randomColor(){
   return createjs.Graphics.getRGB(r,g,b);
 }
 
-function randomPosition(x, y, w, h) {
-  return { x: _.random(x, x + w), y: _.random(y, y + h) };
+function randomPosition(bounds) {
+  return {
+    x: _.random(bounds.x, bounds.x + bounds.w),
+    y: _.random(bounds.y, bounds.y + bounds.h)
+  };
 }
 
 function randomVelocity() {
@@ -34,8 +56,12 @@ function randomVelocity() {
 
 function randomPerson(width, height) {
   var radius   = _.random(35, 50);
-  var position = randomPosition(radius, radius, width - radius, height - radius);
-  return new Person(position, randomVelocity(), radius, randomColor());
+  var bounds   = { x: radius, y: radius, w: width - (radius * 2), h: height - (radius * 2) };
+  var position = randomPosition(bounds);
+  var person   = new Person(position, radius, randomColor());
+  person.bounds   = bounds;
+  person.velocity = randomVelocity();
+  return person;
 }
 
 function randomCrowd(amount, width, height) {
