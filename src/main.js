8183f36fe1450cfd526774ea19e3e1504e04a157
James Reeves <jreeves@weavejester.com>
2013-12-14 10:40:25 -0500

    Use objects to represent Persons

--- src/main.js
+++ src/main.js
@@ -1,15 +1,15 @@
 window.onload = function() {
   var stage = new createjs.Stage("game");
-  person = createPerson();
-  stage.addChild(person.shape);
+  var person1 = new Person(100, 100);
+  var person2 = new Person(100, 300);
+  stage.addChild(person1.shape);
+  stage.addChild(person2.shape);
   stage.update();
 };
 
-function createPerson() {
-  var person = {};
-  person.shape = new createjs.Shape();
-  person.shape.graphics.beginFill("red").drawCircle(0, 0, 50);
-  person.shape.x = 100;
-  person.shape.y = 100;
-  return person;
-}
+var Person = function(x, y) {
+  this.shape = new createjs.Shape();
+  this.shape.graphics.beginFill("red").drawCircle(0, 0, 50);
+  this.shape.x = x;
+  this.shape.y = y;
+};
