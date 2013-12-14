b1087ab9511cb95ab1fa348c6279c9176b0d2d83
James Reeves <jreeves@weavejester.com>
2013-12-14 15:40:15 -0500

    Small cleanup of person.js

--- src/person.js
+++ src/person.js
@@ -4,13 +4,13 @@ var Person = function(x, y, r, col) {
   this.shape.x = x;
   this.shape.y = y;
   var person = this;
-  this.shape.addEventListener("click", function(e) { Game.kill(person); })
+  this.shape.addEventListener("click", function(e) { Game.kill(person); });
 };
 
 // returns a random colour string for beginFill() and other methods.
 // Limited to avoid being too close to pure black and pure white
 function randomColor(){
-  var r = _.random(32, 224)
+  var r = _.random(32, 224);
   var g = _.random(32, 224);
   var b = _.random(32, 224);
   return createjs.Graphics.getRGB(r,g,b);
@@ -30,9 +30,9 @@ function randomCrowd(amount, width, height) {
 function addCrowd(stage, amount) {
   var crowd = randomCrowd(amount, stage.canvas.width, stage.canvas.height);
   _.each(crowd, function(p) { stage.addChild(p.shape) });
-  setPersonAsTarget(_.first(crowd));
+  _.first(crowd).setAsTarget();
 }
-  
-function setPersonAsTarget(person){
-  person.isTarget=true;
-}
+
+Person.prototype.setAsTarget = function() {
+  this.isTarget = true;
+};
